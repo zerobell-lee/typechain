e75e9fd71badf7ecccffe9eb51a70944412c218e
zerobell-lee <bnb3456@gmail.com>
2019-04-02 20:29:27 +0900

    second blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => {
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    }
+
     constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
         this.index = index;
         this.hash = hash;
@@ -15,8 +22,10 @@ class Block {
 
 const genesisBlock = new Block(0, "2020220023920392039", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock]; // 타입 지정에는 [Block] 식으로 특정 자료형만 받는 array를 강제할 수 있다.
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimestamp = (): number => Math.round(new Date().getTime()/1000);
 
 export {};
